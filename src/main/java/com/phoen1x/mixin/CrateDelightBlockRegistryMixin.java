752597bdc8501bb39a0ed5ba1dbed3835f4f0c36
Ruslan <ruslanmur2007@gmail.com>
2025-08-05 21:26:58 +0300

    Initial commit

---
+++ src/main/java/com/phoen1x/mixin/CrateDelightBlockRegistryMixin.java
@@ -0,0 +1,58 @@
+package com.phoen1x.mixin;
+
+import com.axperty.cratedelight.CrateDelight;
+import com.axperty.cratedelight.registry.BlockRegistry;
+import com.phoen1x.impl.block.CrateDelightBaseFactoryBlock;
+import com.phoen1x.impl.block.CrateDelightStatePolymerBlock;
+import com.phoen1x.impl.item.CrateDelightPolyBaseItem;
+import eu.pb4.factorytools.api.block.model.generic.BlockStateModelManager;
+import eu.pb4.polymer.blocks.api.BlockModelType;
+import eu.pb4.polymer.common.api.PolymerCommonUtils;
+import eu.pb4.polymer.core.api.block.PolymerBlock;
+import eu.pb4.polymer.core.api.item.PolymerItem;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.function.Function;
+
+@Mixin(BlockRegistry.class)
+public class CrateDelightBlockRegistryMixin {
+    @Inject(method = "registerBlock(Ljava/lang/String;Ljava/util/function/Function;Lnet/minecraft/block/AbstractBlock$Settings;)Lnet/minecraft/block/Block;", at = @At("TAIL"))
+    private static void polymerify(String path, Function<AbstractBlock.Settings, Block> factory, AbstractBlock.Settings settings, CallbackInfoReturnable<Block> cir) {
+        polymerify(path, cir.getReturnValue());
+    }
+
+    @Unique
+    private static void polymerify(String path, Block block) {
+        Identifier blockId = Identifier.of(CrateDelight.MODID, path);
+        BlockStateModelManager.addBlock(blockId, block);
+        PolymerBlock overlay = null;
+        if (path.endsWith("_crate")) {
+            overlay = CrateDelightStatePolymerBlock.of(block, BlockModelType.FULL_BLOCK);
+        }
+        else if (path.endsWith("_bag")) {
+            overlay = CrateDelightStatePolymerBlock.of(block, BlockModelType.FULL_BLOCK);
+        }
+        if (overlay == null) {
+            if (block.getDefaultState().getCollisionShape(PolymerCommonUtils.getFakeWorld(), net.minecraft.util.math.BlockPos.ORIGIN).isEmpty()) {
+                overlay = CrateDelightBaseFactoryBlock.BARRIER;
+            }
+        }
+        PolymerBlock.registerOverlay(block, overlay);
+    }
+
+    @Inject(method = "registerItem(Ljava/lang/String;Ljava/util/function/Function;Lnet/minecraft/item/Item$Settings;)Lnet/minecraft/item/Item;", at = @At("TAIL"), cancellable = true)
+    private static void polymerify(String path, Function<Item.Settings, Item> factory, Item.Settings settings, CallbackInfoReturnable<Item> cir) {
+        PolymerItem polymerItem;
+        var item = cir.getReturnValue();
+        polymerItem = new CrateDelightPolyBaseItem(item);
+        PolymerItem.registerOverlay(item, polymerItem);
+    }
+}
